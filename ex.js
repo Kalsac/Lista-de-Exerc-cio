d56fd5eb519031cfe253007afd7a6bf4f29d8d79
Isaac/ Ketellin <oliveira.ketellin@escola.pr.gov.br>
2025-05-06 13:50:31 +0000

    as

--- ex.js
+++ ex.js
@@ -1,1 +1,27 @@
+let nome = "Isaac"
+console.log(`Olá, meu nome é ${nome}.`);
 
+let idade = "16", cidade ="Almirante Tamandaré"
+console.log(`Tenho ${idade} anos e moro em ${cidade}.`);
+
+let comida1 = "café", comida2 = "macarrão", comida3 = "chocolate meio amargo"
+console.log(`Minhas comidas favoritas são: ${comida1}, ${comida2} e ${comida3}.`);
+
+let preco = "10,00"
+console.log(`O produto custa R$ ${preco}.`);
+
+let produto = "chocolate meio amargo", quantidade = "3"
+console.log(`Comprei ${quantidade} unidades de ${produto}.`);
+
+6
+
+7
+
+let curso = "desenvolvimento de sistema", instituicao = "Colegio Ambrosio Bini"
+console.log(`Estou cursando ${curso} no ${instituicao}.`);
+
+let cor1 = "verde", cor2 = "marrom", cor3 ="preto"
+console.log(`minhas cores preferidas são ${cor1}, ${cor2} e ${cor3}`);
+
+let mensagem = "passar tempo com meus amigos"
+console.log(`Posso afirmar com certeza de que amo ${mensagem}.`)
